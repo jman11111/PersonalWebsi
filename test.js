01d31c83b65f1654977ae0b2c3bc4dff249ff738
jack <jbroncato@luc.edu>
2020-07-10 00:48:03 -0500

    Sql connection

--- test.js
+++ test.js
@@ -1,15 +1,37 @@
 //Backend for website
 //Website will have searchable images page, as well as main page with links and such
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    database : 'project2',
+    user     : 'root',
+    password : 'root',
+});
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM product', function (error, results, fields) {
+    if (error)
+        throw error;
+
+    results.forEach(result => {
+        console.log(result);
+    });
+});
+
+connection.end();
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
